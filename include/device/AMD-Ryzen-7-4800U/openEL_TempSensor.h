7abb9759a6b8c612299f0729712e315e1b14c357
Fauzan Mahfuzh <129726768+fauzanmahfuzh-uti@users.noreply.github.com>
2023-09-07 16:49:07 +0900

    CPU温度監視プログラムのincludeアップロード

---
+++ include/device/AMD-Ryzen-7-4800U/openEL_TempSensor.h
@@ -0,0 +1,13 @@
+/*
+ * openEL_TempSensor.h
+ *
+ *  Created on: 2023/09/07
+ *      Author: OpenEL-WG
+ */
+
+#ifndef OPENEL_TEMPSENSOR_H_
+#define OPENEL_TEMPSENSOR_H_
+
+extern HAL_FNCTBL_T HalTempSen001Tbl;
+
+#endif /* OPENEL_TEMPSENSOR_H_ */
